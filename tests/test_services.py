8ca3f8d7233df9d092ed4ede994731820709577e
Alexander Skiridomov <n1nj4z33@gmail.com>
2023-06-01 13:22:26 +0300

    fix service tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -3,10 +3,10 @@ import requests
 
 
 @hoverfly("users")
-def test_user_1():
+def test_service_1():
     assert requests.get("https://localhost:8500/services/1").status_code == 200
 
 
 @hoverfly("users")
-def test_user_2():
+def test_service_2():
     assert requests.get("https://localhost:8500/services/2").status_code == 200
